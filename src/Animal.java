6a768dadc5da5db0704e81f39c59bb2b7c6c0850
houdek2 <houdek2@spsejecna.cz>
2024-03-11 08:59:56 +0100

    added farmhouse ArrayList

--- src/Animal.java
+++ src/Animal.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+
 public abstract class Animal {
 
     protected String type;
     protected String name;
     protected Size size;
     protected String specialCare;
+
+    ArrayList<Animal> farmhouse = new ArrayList<>();
 
     @Override
     public String toString() {
