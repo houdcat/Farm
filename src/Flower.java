a56cbb210a2a948bbe78cfbbefff790c3336b0d0
meitnerova <meitnerova@spsejecna>
2024-03-11 08:12:29 +0100

    added Tulip class and ArrayList for Flowers

--- src/Flower.java
+++ src/Flower.java
@@ -1,9 +1,16 @@
+import java.util.ArrayList;
+
 public abstract class Flower {
 
     protected String name;
     protected double price;
     protected double neededArea;
     protected double chanceOfGrowth;
+
+    ArrayList<Flower> flowers = new ArrayList<>();
+    public void plantFlower(){
+
+    }
 
     @Override
     public String toString() {
